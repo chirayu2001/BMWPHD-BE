048b80103a80d6edc065bb9932748e852196d6fa
chirayu2001 <chirayu.jain@tcu.edu>
2023-01-27 22:16:07 -0600

    base64 error resolution

--- src/main/java/net/bmw/bmwphd/configuration/SecurityConfiguration.java
+++ src/main/java/net/bmw/bmwphd/configuration/SecurityConfiguration.java
@@ -86,14 +86,14 @@ public class SecurityConfiguration {
 //        RSAPrivateKey priv = myPrivateKeyConverter.convert(privKeyString);
        // pubKeyString = pubKeyString.replaceAll("[\\s|\\t|\\r\\n]+","").trim();
         //privKeyString = privKeyString.replaceAll("\\n","");
-        byte[] privBytes = privKeyString.getBytes(StandardCharsets.UTF_8);
+        //byte[] privBytes = privKeyString.getBytes(StandardCharsets.UTF_8);
         X509EncodedKeySpec keySpecX509 = new X509EncodedKeySpec(Base64.getDecoder().decode(pubKeyString));
         RSAPublicKey myKey = (RSAPublicKey) kf.generatePublic(keySpecX509);
         System.out.println("after conversion Public: " + myKey.toString());
         //RSAPublicKey myKey = myPublicKeyConverter.convert(pubKeyString);
 
-        PKCS8EncodedKeySpec keySpecPKCS8 = new PKCS8EncodedKeySpec(privBytes);
-        //PKCS8EncodedKeySpec keySpecPKCS8 = new PKCS8EncodedKeySpec(Base64.getDecoder().decode(privKeyString));
+        //PKCS8EncodedKeySpec keySpecPKCS8 = new PKCS8EncodedKeySpec(privBytes);
+        PKCS8EncodedKeySpec keySpecPKCS8 = new PKCS8EncodedKeySpec(Base64.getDecoder().decode(privKeyString));
         RSAPrivateKey priv2 = (RSAPrivateKey) kf.generatePrivate(keySpecPKCS8);
 
 
