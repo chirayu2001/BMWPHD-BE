75db1d35802a92a07057971521c4b6d85eed6a38
chirayu2001 <chirayu.jain@tcu.edu>
2023-02-08 12:28:17 -0600

    ignore case in name

--- src/main/java/net/bmw/bmwphd/service/HorseService.java
+++ src/main/java/net/bmw/bmwphd/service/HorseService.java
@@ -43,7 +43,7 @@ public class HorseService {
                     predicateList.add(cb.like(root.get("id").as(String.class), "%" + (String) searchMap.get("id") + "%"));
                 }
                 if (searchMap.get("name") != null && !"".equals(searchMap.get("name"))) {
-                    predicateList.add(cb.like(root.get("name").as(String.class), "%" + (String) searchMap.get("name") + "%"));
+                    predicateList.add(cb.like(root.get("name").as(String.class), "%" + (String) searchMap.get("name").toString().toLowerCase() + "%"));
                 }
                 if (searchMap.get("dam1") != null && !"".equals(searchMap.get("dam1"))) {
                     predicateList.add(cb.like(root.get("dam1").as(String.class), "%" + (String) searchMap.get("dam1") + "%"));
