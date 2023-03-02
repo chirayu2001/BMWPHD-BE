8767cc4c83f7eb604c200bebe18fb3c6f405fbcc
chirayu2001 <chirayu.jain@tcu.edu>
2023-03-01 22:12:04 -0600

    open ssll page and remove local key files and remove IdWorker

--- src/main/java/net/bmw/bmwphd/BmwPhdApplication.java
+++ src/main/java/net/bmw/bmwphd/BmwPhdApplication.java
@@ -2,18 +2,12 @@ package net.bmw.bmwphd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BmwPhdApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BmwPhdApplication.class, args);
-    }
-
-    @Bean
-    public IdWorker idWorker() {
-        return new IdWorker(1, 1);
     }
 
 }
